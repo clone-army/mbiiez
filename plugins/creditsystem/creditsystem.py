07d8d39e49305ea0925725e04bdc9997bd297397
Louis Varley <30378727+louisvarley@users.noreply.github.com>
2026-05-14 00:49:37 +0100

    Update creditsystem.py

--- plugins/creditsystem/creditsystem.py
+++ plugins/creditsystem/creditsystem.py
@@ -15,6 +15,9 @@ class plugin:
 
         if(self.instance.has_plugin("auto_message")):
             self.instance.config['plugins']['auto_message']['messages'].append("Credit system is enabled on this server.")
+            self.instance.config['plugins']['auto_message']['messages'].append("!balance - Shows your current credits and any bounty on you.")
+            self.instance.config['plugins']['auto_message']['messages'].append("!buy - Lists available items or buys the selected one by ID.")
+            self.instance.config['plugins']['auto_message']['messages'].append("!bounty - Places a credit bounty on another player.")
 
     def register(self):
         self.instance.process_handler.register_service("Credit System Service", self.credit_system_service)
