95731957c7f2223910b55c3d20d471b3335b88c7
Louis Varley <30378727+louisvarley@users.noreply.github.com>
2026-05-15 13:11:51 +0100

    debug steps

--- mbiiez/web/controllers/dashboard.py
+++ mbiiez/web/controllers/dashboard.py
@@ -1,10 +1,18 @@
-from mbiiez.web.tools import tools
+from mbiiez.web.tools import tools, trace_dashboard_load
+import time
 
 class controller:
     controller_bag = {}
 
     def __init__(self):
+        controller_start = time.perf_counter()
+        trace_dashboard_load("dashboard controller start")
         names = tools().list_of_instances()
+        trace_dashboard_load(
+            "dashboard controller instances loaded",
+            "count={}".format(len(names)),
+            (time.perf_counter() - controller_start) * 1000,
+        )
         self.controller_bag = {
             'instances': [{'name': name} for name in names],
             'summary': {
@@ -13,3 +21,7 @@ class controller:
                 'players': 0,
             }
         }
+        trace_dashboard_load(
+            "dashboard controller end",
+            elapsed_ms=(time.perf_counter() - controller_start) * 1000,
+        )
